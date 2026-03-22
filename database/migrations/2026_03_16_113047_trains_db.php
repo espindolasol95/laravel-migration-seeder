1638e674778e8793124b2661d92f188fbb2b44a6
espindolasol95 <espindolasol.ok@gmail.com>
2026-03-22 14:41:55 +0100

    feat [Definizione campi dentro il file della migratrion]

--- database/migrations/2026_03_16_113047_trains_db.php
+++ database/migrations/2026_03_16_113047_trains_db.php
@@ -11,7 +11,24 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('trains', function (Blueprint $table) {
+
+            $table->id();
+            $table->string('azienda');
+            $table->string('stazione_partenza');
+            $table->string('stazione_arrivo');
+            //qui usero datatime per gestire data e ora insieme
+            $table->dataTime('orario_partenza');
+            $table->datatime('orario_arrivo');
+            //qui usero uniche come codicde unico per evitare duplicati
+            $table->string('codice_treno')->uniche();
+            //interger -> numero intero
+            $table->integer('totale_carrozze');
+            //booleano valore vero o falso
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
+            $table->timestamps();
+        });
     }
 
     /**
